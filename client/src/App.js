39bfa9169a76a528dae9a7b886cf16ea7d974668
gayathiri <gayathirie.21cse@kongu.edu>
2024-07-05 17:46:45 +0530

    initila

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Login from './components/login'
+import "./App.css"
+import {useStateValue} from "./components/contextApi/StateProvider"
 
 const App=() =>{
+const [{user}] =useStateValue();
+
   return (
-    <div>
-      <Login/>
+    <div className='app'>
+      {!user ?
+      <Login/>:
+      <h1>Loggedin</h1>
+      }
     </div>
-  )
-}
+  );
+};
 
 export default App
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,24 +2,34 @@ import React from 'react'
 import {Button} from "@mui/material/Button"
 import {auth,provider} from "../firebase";
 import {signInWithPopup} from "firebase/auth"
-import "./login.css"
+import "./login.css";
+import {useStateValue} from "./contextApi/StateProvider";
+import {actionTypes} from "./contextApi/reducer";
 
-const login =()  =>{
+const Login =()  =>{
+
+    const [state,dispatch] =useStateValue();
+    console.log(state);
 
     const signIn = ()=>{
-        signInWithPopup(auth,provider).then((result)=>{
-            console.log(result);
-        }).catch((err)=>{
-            alert(err.message)
+        signInWithPopup(auth,provider)
+        .then((result)=>{
+           dispatch({
+            type:actionTypes.SET_USER,
+            user:result.user,
+           });
         })
-    }
+        .catch((err)=>{
+            alert(err.message)
+        });
+    };
 
   return (
     <div className='login'>
         <div className='login_container'>
             <img src="https://tse3.mm.bing.net/th?id=OIP.yNlvU1fv5eA1Hmx8Toi_YQHaHn&pid=Api&P=0&h=180" alt="logo"/>
             <div className='login_text'>
-                <h1>Sign in to Whatsapp</h1>
+                <h1>SignIn to Whatsapp</h1>
             </div>
             <button onClick={signIn}>
                 Sign In with Google
@@ -29,4 +39,4 @@ const login =()  =>{
   )
 }
 
-export default login
+export default Login;
